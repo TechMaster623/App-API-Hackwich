ee447a92b5cb1ec9fcd0170f26b3855799df616c
Faiz Ali <fali@bsd220.org>
2023-02-21 09:49:28 -0600

    Initial Commit

---
+++ App API Hackwich/App_API_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_API_HackwichApp.swift
+//  App API Hackwich
+//
+//  Created by Faiz Ali on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct App_API_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
